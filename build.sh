e98bba4a3e39da0f7d5d218cefad3ecaf997a229
Adam Harrison <adamdharrison@gmail.com>
2024-10-06 23:37:51 -0400

    Fixed build.sh

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 : ${CC=gcc}
 : ${BIN=libremotestream.so}
 
-CFLAGS="$CFLAGS -fPIC -Ilib/lite-xl/resources/include"
+CFLAGS="$CFLAGS -fPIC -Ilib/lite-xl/resources/include -Ilib/zstd/lib"
 LDFLAGS=""
 
 if [[ ! -e "zstd.o" ]]; then
-  cd lib/zstd/build/single_file_libs && ./combine.sh -r ../../lib -x legacy/zstd_legacy.h -k zstd.h -o zstd.c zstd-in.c && gcc -c $CFLAGS $@ zstd.c -o ../../../../zstd.o;  cd -
+  cd lib/zstd/build/single_file_libs && ./combine.sh -r ../../lib -x legacy/zstd_legacy.h -k zstd.h -o zstd.c zstd-in.c && $CC -c $CFLAGS $@ zstd.c -o ../../../../zstd.o;  cd -
 fi
 
 [[ "$@" == "clean" ]] && rm -f *.so *.dll *.o && exit 0
